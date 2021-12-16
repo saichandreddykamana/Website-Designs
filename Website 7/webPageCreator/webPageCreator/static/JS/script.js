19ddda25789911a519032beadc1d57508e60ed9f
Sai Chand Reddy Kamana <saichand.kamana@outlook.com>
2021-12-16 11:44:58 +0000

    added email validation at back-end.

--- Website 7/webPageCreator/webPageCreator/static/JS/script.js
+++ Website 7/webPageCreator/webPageCreator/static/JS/script.js
@@ -14,9 +14,14 @@ $('#demo-form').on('submit', function(e){
               document.getElementById('demo-response').innerHTML = "<p>There is an existing demo booked with this email <br> <b>E-Mail : </b>" + customer[0].fields.customer_mail + " <br> <b>DATE : </b>" + customer[0].fields.demo_date + ".</p>";
               document.getElementById('demo-response').style.background = 'red';
             }else{
-              customer = JSON.parse(data.customer_details);
-              document.getElementById('demo-response').innerHTML = "<p>demo is booked with this email <br> <b>E-Mail : </b>" + customer[0].fields.customer_mail + " <br> <b>DATE : </b>" + customer[0].fields.demo_date + ".</p>";
-              document.getElementById('demo-response').style.background = 'green';
+              if(data.customer_details != 'Please enter valid E-Mail Address.'){
+                customer = JSON.parse(data.customer_details);
+                document.getElementById('demo-response').innerHTML = "<p>demo is booked with this email <br> <b>E-Mail : </b>" + customer[0].fields.customer_mail + " <br> <b>DATE : </b>" + customer[0].fields.demo_date + ".</p>";
+                document.getElementById('demo-response').style.background = 'green';
+              }else{
+                document.getElementById('demo-response').innerHTML = "<p>"+ data.customer_details + "</p>";
+                document.getElementById('demo-response').style.background = 'red';
+              }
             }
            },
 
--- Website 7/webPageCreator/webPageCreator/views.py
+++ Website 7/webPageCreator/webPageCreator/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, JsonResponse
 from django.core import serializers
 from webPageCreator.models import demoCustomers
 from datetime import datetime, timedelta
+import re
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', "webPageCreator.settings")
 
@@ -16,14 +17,18 @@ def schedule_demo(request):
     exists = False
     if request.method == 'POST':
         customer_mail = request.POST['customer_mail']
-        customer = demoCustomers.objects.filter(customer_mail=customer_mail)
-        if customer.exists():
-            customer_details = serializers.serialize("json", customer)
-            exists = True
-        else:
-            customer = demoCustomers.objects.create(customer_mail = customer_mail, demo_date = datetime.now() + timedelta(days=1))
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        if re.fullmatch(regex,customer_mail):
             customer = demoCustomers.objects.filter(customer_mail=customer_mail)
-            customer_details = serializers.serialize("json", customer)
+            if customer.exists():
+                customer_details = serializers.serialize("json", customer)
+                exists = True
+            else:
+                customer = demoCustomers.objects.create(customer_mail = customer_mail, demo_date = datetime.now() + timedelta(days=1))
+                customer = demoCustomers.objects.filter(customer_mail=customer_mail)
+                customer_details = serializers.serialize("json", customer)
+        else:
+            customer_details = 'Please enter valid E-Mail Address.'
     response = {'customer_details': customer_details, 'exists': exists}
     return JsonResponse(response)
     
