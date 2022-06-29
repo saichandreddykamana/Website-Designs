73e733db350d7e71a3320bc360aa6e4822f670f5
Sai Chand Reddy Kamana <saichand.kamana@outlook.com>
2022-06-29 11:47:54 +0100

    setting calendar layout.

--- SpaceX Calendar/SpaceXCalendar.js
+++ SpaceX Calendar/SpaceXCalendar.js
@@ -10,7 +10,21 @@ function getUpcomingLaunches(){
         request.onload = async function() {
             if(request.status == 200){
                 all_launches = JSON.parse(this.response);
-                document.getElementById('all-launches').innerText = JSON.stringify(all_launches);
+                changeCalenderLayout();
             }
         }
+}
+
+// function to set the calendar layout
+function changeCalenderLayout(){
+    var month = document.getElementById('month');
+    var noOfDays = 30;
+    if (month.selectedIndex % 2 == 0){
+        noOfDays = 31;
+    }
+    var calendar = "";    
+    for(var i = 0 ; i < noOfDays ; i++){
+        calendar += "<span class='day''>" + (i + 1) + "</span>"; 
+    }
+    document.getElementById('calendar-layout').innerHTML = calendar;
 }
